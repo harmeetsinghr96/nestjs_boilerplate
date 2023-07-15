25973e5dcf148e53a9662c7e8fe1cb350ce9a93d
harmeet.singh <harmeet.singh@antiersolutions.com>
2023-07-15 14:24:02 +0530

    fixed env modules in .env.deelopment

--- services/gateway_service/src/main.ts
+++ services/gateway_service/src/main.ts
@@ -2,19 +2,23 @@ import helmet from 'helmet';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger();
-  const host = process.env.HOST_NAME || '127.0.0.1';
-  const port = process.env.PORT || 8081;
   /** initializing application */
   const app = await NestFactory.create(AppModule);
+  /** initialize confgi service to get excess all the env's local */
+  const configService = app.get<ConfigService>(ConfigService);
+  /** initialize middlewares */
   app.enableCors();
-  app.use(
-    helmet({
-      crossOriginEmbedderPolicy: false,
-    }),
-  );
+  app.enableVersioning();
+  app.use(helmet({ crossOriginEmbedderPolicy: false }));
+  /** initialize host and port */  app.setGlobalPrefix(configService.get('SERVICE_NAME'));
+
+  const host = process.env.HOST_NAME || configService.get('HOST_NAME');
+  const port = process.env.PORT || configService.get('PORT');
+  /** start application on host and port */
   await app.listen(port, host);
   /** print log if server has been started */
   logger.log(`Gateway service is listening on ${host}:${port}`);
